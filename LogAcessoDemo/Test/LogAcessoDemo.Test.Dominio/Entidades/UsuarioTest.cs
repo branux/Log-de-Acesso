65f25734233cbcce01c8d3b2f7e9bd1ade5cd755
fnalin <fnalin@msn.com>
2015-08-30 15:31:34 -0300

    Subindo o projeto

---
+++ LogAcessoDemo/Test/LogAcessoDemo.Test.Dominio/Entidades/UsuarioTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogAcessoDemo.Test.Dominio.Entidades
+{
+    [TestClass]
+    public class UsuarioTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ LogAcessoDemo/app/LogAcessoDemo.App.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,11 @@
+﻿
+namespace LogAcessoDemo.App.Dominio.Entidades
+{
+    public class Usuario
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+    }
+}
