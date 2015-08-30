1d18fff51e02ecca3e8b74d1cd252553c28b12a6
fnalin <fnalin@msn.com>
2015-08-30 15:57:00 -0300

    Add entidadeusuariotest

---
+++ LogAcessoDemo/Test/LogAcessoDemo.Test.Dominio/Entidades/EntidadeUsuarioTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LogAcessoDemo.App.Dominio.Entidades;
+
+namespace LogAcessoDemo.Test.Dominio.Entidades
+{
+    [TestClass]
+    public class EntidadeUsuarioTest
+    {
+
+        //DADO UM USUÁRIO
+
+        [TestMethod]
+        public void Deverá_herdar_da_EntidadeBase()
+        {
+            Assert.IsTrue(typeof(Usuario).IsSubclassOf(typeof(EntidadeBase)), "Entidade não derivada da EntidadeBase");
+        }
+
+
+        [TestMethod]
+        public void Não_poderá_ser_validado_se_nome_for_nulo_ou_branco()
+        {
+            //arrange
+            var usuario = new Usuario(null, "fnalin", "123@qwe");
+
+            //act
+            var usuarioEhValido = usuario.Validar();
+
+            //assert
+            Assert.AreEqual(false, usuarioEhValido);
+        }
+    }
+}
