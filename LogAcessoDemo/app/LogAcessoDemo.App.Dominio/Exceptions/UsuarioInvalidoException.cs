b2c05b2e13d85911fb2759f67f128d6dc128f424
fnalin <fnalin@msn.com>
2015-09-01 13:53:52 -0300

    Adicionando UsuarioException ao git

---
+++ LogAcessoDemo/app/LogAcessoDemo.App.Dominio/Exceptions/UsuarioInvalidoException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace LogAcessoDemo.App.Dominio.Exceptions
+{
+    public class UsuarioInvalidoException : System.Exception
+    {
+        public UsuarioInvalidoException(string mensagem)
+            : base(mensagem)
+        { }
+    }
+}
