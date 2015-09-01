8d8d125ddf8a383e16d37902a9d7e22849d62dd3
fnalin <fnalin@msn.com>
2015-09-01 16:02:19 -0300

    add IAutenticacaoServico

---
+++ LogAcessoDemo/app/LogAcessoDemo.App.Dominio/Interfaces/Servico/IAutenticacaoServico.cs
@@ -0,0 +1,8 @@
+﻿
+namespace LogAcessoDemo.App.Dominio.Interfaces.Servico
+{
+    public interface IAutenticacaoServico : System.IDisposable
+    {
+        Entidades.Usuario ObterDadosAposAutenticando(string login, string senha);
+    }
+}
